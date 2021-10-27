71e62feaf7aa4dfe5d9052c616fb59e20c2c17ef
Ben Whalley <ben247@gmail.com>
2021-10-27 11:23:34 +0100

    Update blog subtitle

--- partials/subtitle.php
+++ partials/subtitle.php
@@ -13,9 +13,9 @@
         <?php 
                 
                 if( is_page(29)) {
-                    echo esc_html__("I'm a developer who enjoys working in front-end development but also gets a kick out of back-end development");
+                    echo esc_html__("I'm a developer who enjoys working on front-end projects but also gets a kick out of back-end development");
                 } elseif ( is_page(43)) {
-                    echo esc_html__("A repository for some useful code snippets");
+                    echo esc_html__("A repository for my brain & some useful code snippets");
                 } elseif ( is_page(14)) {
                     echo esc_html__("Hi, I'm Ben, a Lancashire-based freelance web developer, specialising in WordPress development and front-end design");
                 } elseif ( is_page(39)) {
