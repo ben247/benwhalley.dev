5d0a962b2ad96140cb4aac0e8a1e63472f51458c
Ben Whalley <ben247@gmail.com>
2021-11-06 11:42:34 +0000

    Updated services layout to bootstrap

--- partials/services.php
+++ partials/services.php
@@ -8,55 +8,75 @@
  */
 ?>
 
-<div class="services-grid">
-
-    <div class="sg-item">
-        <h2 class="lined">What I do.</h2>
-        <div class="services-img"><img src="<?php echo get_template_directory_uri(); ?>/images/wp-logo.png"
-                alt="Wordpress Logo"></div>
-        <div class="services-img"><img src="<?php echo get_template_directory_uri(); ?>/images/php-logo.png"
-                alt="PHP Logo"></div>
-        <div class="services-img"><img src="<?php echo get_template_directory_uri(); ?>/images/woocommerce-logo.png"
-                alt="Woocommerce Logo">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <h2 class="lined">
+                What I do.
+            </h2><img alt="Wordpress Logo" src="<?php echo get_template_directory_uri(); ?>/images/wp-logo.png" />
+        </div>
+        <div class="col-md-4">
+            <h3>
+                Front-end Development
+            </h3>
+            <p>
+                I build websites with HTML, CSS and a little jQuery to build responsive user friendly websites.
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h3>
+                WordPress Development
+            </h3>
+            <p>
+                I work with WordPress, a powerful Content Management System which allows full management of the site’s
+                content.
+            </p>
         </div>
     </div>
-
-    <div class="sg-item">
-        <div class="service-block">
-            <h3>Front-end Development</h3>
-            <p>I build websites with HTML, CSS and a little jQuery to build responsive user friendly websites.</p>
+    <div class="row">
+        <div class="col-md-4">
+            <img alt="PHP Logo" src="<?php echo get_template_directory_uri(); ?>/images/php-logo.png" />
         </div>
-        <div class="service-block">
-            <h3>WordPress Development</h3>
-            <p>I work with WordPress, a powerful Content Management System which allows full management of the site’s
-                content.</p>
+        <div class="col-md-4">
+            <h3>
+                Responsive Web Design
+            </h3>
+            <p>
+                All my builds are responsive, this means it will always fit the size of the screen it’s viewed on
+                offering all users a great experience.
+            </p>
         </div>
-        <div class="service-block">
-            <h3>Responsive Web Design</h3>
-            <p>All my builds are responsive, this means it will always fit the size of the screen it’s viewed on
-                offering all users a great experience.</p>
-        </div>
-    </div>
-
-    <div class="sg-item">
-        <div class="service-block">
-            <h3>Maintenance</h3>
-            <p>I can keep your WordPress website secure and up-to-date, fix issues with existing themes and plugins, or
+        <div class="col-md-4">
+            <h3>
+                Maintenance
+            </h3>
+            <p>
+                I can keep your WordPress website secure and up-to-date, fix issues with existing themes and plugins, or
                 upgrade PHP to better support your website.
             </p>
         </div>
-        <div class="service-block">
-            <h3>E-Commerce</h3>
-            <p>I create a functional online shopping presence for small and medium companies. I mostly work with the
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <img alt="Woocommerce Logo" src="<?php echo get_template_directory_uri(); ?>/images/woocommerce-logo.png" />
+        </div>
+        <div class="col-md-4">
+            <h3>
+                E-Commerce
+            </h3>
+            <p>
+                I create a functional online shopping presence for small and medium companies. I mostly work with the
                 WordPress
                 plugin WooCommerce.
             </p>
         </div>
-        <div class="service-block">
-            <h3>Migrations</h3>
-            <p>I can take the stress out of migrating your website between hosts or to WordPress from another supplier.
+        <div class="col-md-4">
+            <h3>
+                Migrations
+            </h3>
+            <p>
+                I can take the stress out of migrating your website between hosts or to WordPress from another supplier.
             </p>
         </div>
     </div>
-
 </div>
